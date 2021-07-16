dfaf09f0d4f646f197f53990db274ce9209a1a7a
TsykalanovDima <86851333+TsykalanovDima@users.noreply.github.com>
2021-07-16 12:14:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function mainFunction(callback) {
+	let a = +prompt('nubmer1')
+
+	let b = +prompt('nubmer 2')
+
+	return callback (a,b)
+}
+
+function exponentiation(a,b) {
+	return a**b
+	}
+ 
+	2
+	
+function exponentiation(a,b) {
+		return a/b
+	}
+	
+
+alert(mainFunction(exponentiation))
